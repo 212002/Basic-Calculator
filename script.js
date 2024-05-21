7530564185e7d7a8df6bd3036defaa24c2f4f38b
Ambika Ananda Poojary <88726561+212002@users.noreply.github.com>
2024-05-21 20:37:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        alert('Invalid expression');
+        clearDisplay();
+    }
+}
